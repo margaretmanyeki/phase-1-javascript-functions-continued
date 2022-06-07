835f865a94391d509d1596d0459f71a0faec7912
margaret manyeki <maggiemanyeki6@gmail.com>
2022-06-08 00:06:59 +0300

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,8 @@ return `This Saturday, I want to ${fun}!`;
 function mondayWork(activity="go to the office"){
     return `This Monday, I will ${activity}.`;
 }
+function wrapAdjective(comment='*'){
+ return function(attr = "special"){
+     return `You are ${comment}${attr}${comment}!`
+ }
+}
