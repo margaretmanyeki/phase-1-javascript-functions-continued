1bd62f460c0e33dc831c018868078d5481d6aa5a
margaret manyeki <maggiemanyeki6@gmail.com>
2022-06-07 23:17:12 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // code your solution here
+function saturdayFun(fun="roller-skate"){
+return `This Saturday, I want to ${fun}!`;
+}
+function mondayWork(activity="go to the office"){
+    return `This Monday, I will ${activity}.`;
+}
